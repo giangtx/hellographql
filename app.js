8394124f368fb86df42774c3a308d5c5f752963a
giangtx <36334907+giangtx@users.noreply.github.com>
2023-04-17 11:36:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import logger from 'morgan';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './schema';
 import mongoose from 'mongoose';
-import dbConfig from './config/db.config'
+import dbConfig from './config/db.config';
+import http from "http";
+var debug = require('debug')('graphql:server');
 
 const app = express();
 
@@ -55,5 +57,85 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3003');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+const onListening = ()=> {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+const onError = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.log(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.log(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
 
 module.exports = app;
